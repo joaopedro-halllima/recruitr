508b226d93b1a4cc47c23566b718960d24decd54
joaopedro-halllima <158372513+joaopedro-halllima@users.noreply.github.com>
2026-05-11 19:58:23 -0400

    Fixing tbls

--- api/alembic/versions/bd04ab0d0138_create_users_roles_coach_profiles.py
+++ api/alembic/versions/bd04ab0d0138_create_users_roles_coach_profiles.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'bd04ab0d0138'
-down_revision: Union[str, Sequence[str], None] = None
+down_revision: Union[str, Sequence[str], None] = "0000_init_all"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
